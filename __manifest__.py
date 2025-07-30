d8b55bacd987ac065e6657748f3b66d2ab8c6c2a
Wanda123 <wandadeheza9@gmail.com>
2025-07-30 19:22:30 -0400

    Agregado pie de página con leyenda en el layout del reporte PDF

--- __manifest__.py
+++ __manifest__.py
@@ -7,7 +7,7 @@
     'author': 'Retail & IT Solutions ft JhonConnor',
     'website': 'https://retailsolutions-bo.com/',
     'category': 'Purchases', 
-    'depends': ['base', 'purchase', 'stock','mail', 'uom','l10n_bo_rs_ice',],
+    'depends': ['base', 'purchase', 'stock','mail', 'uom'],
     'data': [
         'report/report.xml',
         'report/purchase_order_custom_report.xml',
@@ -15,6 +15,7 @@
         'views/stock_picking_view.xml',
         'report/report_template_guia_remision.xml',
         'report/report_template_recepcion.xml',
+        'report/report_template_guia_remision.xml',
     ],
     'installable': True,
     'application': False,
